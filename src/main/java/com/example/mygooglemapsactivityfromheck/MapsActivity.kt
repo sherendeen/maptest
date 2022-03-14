c72ddb8951f3a2906ab63f972717caf8d1c87129
Seth G. R. Herendeen <inivican@hotmail.com>
2022-03-14 15:51:09 -0400

    Add files via upload

---
+++ src/main/java/com/example/mygooglemapsactivityfromheck/MapsActivity.kt
@@ -0,0 +1,118 @@
+package com.example.mygooglemapsactivityfromheck
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.mygooglemapsactivityfromheck.databinding.ActivityMapsBinding
+
+private const val PERMISSION_CODE_REQUEST_LOCATION = 1
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        if ( hasLocationPermission() ) {
+            getLastLocation()
+        } else {
+            requestPermissionWithRationaleIfNeeded()
+        }
+
+
+        // Add a marker in Sydney and move the camera
+        val buffalo = LatLng(42.880230, -78.878738)
+        mMap.addMarker(MarkerOptions().position(buffalo).title("Marker in Buffalo"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(buffalo))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            PERMISSION_CODE_REQUEST_LOCATION -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation()
+            } else {
+                requestPermissionWithRationaleIfNeeded()
+            }
+        }
+    }
+
+    private fun requestPermissionWithRationaleIfNeeded() =
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            showPermissionRationale {
+                requestLocationPermission()
+            }
+        } else {
+            requestLocationPermission()
+        }
+
+    private fun showPermissionRationale(positiveAction: () -> Unit) {
+
+        AlertDialog.Builder(this)
+            .setTitle("Location permission")
+            .setMessage("This app will not work without knowing your current location")
+            .setPositiveButton("OK") { _, _ -> positiveAction() }.create().show()
+
+    }
+
+    private fun hasLocationPermission() =
+        ContextCompat.checkSelfPermission(this,
+        android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_CODE_REQUEST_LOCATION
+        )
+    }
+
+    private fun getLastLocation() {
+        Log.d("MapsActivity", "getLastLocation() called.")
+    }
+
+
+}
